8eef3b36bf9bb3865876f08b7f04c54a385afe15
darlin <duxiaodong@darlin.me>
2017-09-11 12:54:27 +0800

    add global env

--- src/etc/fetch/interceptors/request.js
+++ src/etc/fetch/interceptors/request.js
@@ -1,11 +1,9 @@
 import { Interceptor } from 'intercept-fetch';
 
-const apiPrefix = 'https://api.github.com';
-
 const requestInterceptor = new Interceptor({
     prefix: {
         request(url, config) {
-            return Promise.resolve([apiPrefix + url, config]);
+            return Promise.resolve([ENV.API_PREFIX + url, config]);
         }
     }
 });
