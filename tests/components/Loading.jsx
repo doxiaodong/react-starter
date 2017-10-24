9e3a2b3dcbe144f541481f4d2a29a57b1f4b6d8e
duxiaodong <duxiaodong@darlin.me>
2017-10-24 13:18:53 +0800

    fix test error

--- tests/components/Loading.jsx
+++ tests/components/Loading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import Loading from 'components/Loading';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Loading Component', () => {
     test('Snapshot', () => {
