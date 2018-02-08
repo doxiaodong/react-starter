dfe31d21de718508aa90b6563f3787a0723e918b
darlin <duxiaodong@darlin.me>
2018-02-08 15:44:35 +0800

    use fec-builder@1.7

--- build.sh
+++ build.sh
@@ -3,32 +3,27 @@
 BUILD_ENV=$1
 SSR=$2
 
+BUILD_CONFIG_FILE=build-config/base.json
+
+if [ SSR = true ]; then
+    if [ -f build-config/${BUILD_ENV}.ssr.json ]; then
+        BUILD_CONFIG_FILE=build-config/${BUILD_ENV}.ssr.json
+    else
+        BUILD_CONFIG_FILE=build-config/base.ssr.json
+    fi
+else
+    if [ -f build-config/${BUILD_ENV}.json ]; then
+        BUILD_CONFIG_FILE=build-config/${BUILD_ENV}.json
+    else
+        BUILD_CONFIG_FILE=build-config/base.json
+    fi
+fi
+
 echo "build env: ${BUILD_ENV}"
 echo "is SSR: ${SSR}"
+echo "build-config file: ${BUILD_CONFIG_FILE}"
 
 # prepare
-if [ -f build-config.json ]; then
-    rm build-config.json
-fi
-
-publicUrl="\/"
-if [ $BUILD_ENV = "beta" ]; then
-    publicUrl="https:\/\/beta-static.darlin.me"
-elif [ $BUILD_ENV = "prod" ]; then
-    publicUrl="\/"
-fi
-
-if [ $SSR = "true" ]; then
-    htmlname="index.ssr.html"
-else
-    htmlname="index.html"
-fi
-
-render_template() {
-    sed -e "s/\${publicUrl}/$publicUrl/" -e "s/\${htmlname}/$htmlname/" $1 > $2
-}
-render_template build-config.tpl.json build-config.json
-
 if [ -f server/env.json ]; then
     rm server/env.json
 fi
@@ -36,5 +31,6 @@ cp env/${BUILD_ENV}.json server/env.json
 
 # build
 alias fec-builder=./node_modules/.bin/fec-builder
-fec-builder clean
-fec-builder -e production -f ./env/${BUILD_ENV}.json --ISOMORPHIC_TOOLS_FILE ./server/isomorphic.js generate
+
+fec-builder clean -c ${BUILD_CONFIG_FILE}
+fec-builder -e production -c ${BUILD_CONFIG_FILE} -f ./env/${BUILD_ENV}.json --ISOMORPHIC_TOOLS_FILE ./server/isomorphic.js generate
